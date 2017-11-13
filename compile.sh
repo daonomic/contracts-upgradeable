c5ecccbf3b120700346b802b7eba478c8b4da5d8
0v1se <dev@0v1se.com>
2017-11-13 13:15:42 +0300

    fixes

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+rm -Rf build
+truffle compile
---
+++ flatten.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+solidity_flattener --solc-paths="..=.." --output flat/Dispatcher.sol contracts/upgradeable/Dispatcher.sol
